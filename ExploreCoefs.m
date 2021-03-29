aa12aa8fedcc27b743e330d5152c5558f1803852
J_Afonso <joaorui1982@gmail.com>
2021-03-29 17:37:01 +0100

    Finished exploration of encoding distribution on ExploreCoefs.m

--- ExploreCoefs.m
+++ ExploreCoefs.m
@@ -37,19 +37,15 @@ for m = 1:5
         for c = 1:length(intCond(1,:))
             
             load(['/Volumes/Cerebro/Recording Data/',Name,'/Predict from Neurons/SpdPredResultsTimePaper Cond_',num2str(intCond(1,c)),' TSeg_',num2str(event),' spdSmt_',num2str(spdSmt),' Sigma_', num2str(stdv),' Alpha_',num2str(alpha),'.mat'],...
-                'allPredMat','Laps','dummyLaps','allTrials','coefs');
+                'coefs','allPredMat');
             
-            %             LapsS{c,1} = Laps{1,12};
             
-            for d = 1:length(Laps(1,:))
+            for d = 1:length(coefs(1,:))
                 coefsS{s,d}{c} = coefs{1,d};
+                featsS{s,d}{c} = allPredMat{1,d};
             end
-            %             dummyLapsS{c,1} = dummyLaps{1,12};
-            %             %dummyLapsS{c,1} = (allPredMat{1,12} * coefsS{c,1}(2:end)) + coefsS{c,1}(1);
-            %             outTrialsS{c,1} = allTrials{1,12};
-            %
             
-            clear Laps dummyLaps allTrials coefs allPredMat
+            clear  coefs allPredMat
             
         end
         
@@ -57,42 +53,132 @@ for m = 1:5
         
         
         load(['/Volumes/Cerebro/Recording Data/',Name,'/Predict from Neurons/OutPredResultsTimePaper Cond_',num2str(intCondO),' TSeg_',num2str(eventO),' spdSmt_',num2str(spdSmt),' Sigma_', num2str(stdv),' Alpha_',num2str(alpha),'.mat'],...
-            'Laps','dummyLaps','allTrials','coefs','allDists');
+            'coefs','allPredMat');
         
         
-        
-        %             if sID(c) == 2
-        %                 tType = 0;
-        %             end
-        
-        
-        
-        %             LapsO{c,1} = Laps{1,12}(allTrials{1,12}(:,1) == tType,1);
-        
-        %     if c == 2
-        %         LapsO{c,1} = LapsO{c,1} - 2;
-        %     end
-        
-        %dummyLaps{1,12} = ones(length(dummyLaps{1,12}(:,1)),1) - dummyLaps{1,12}(:,1);
-        %             dummyLapsO{c,1} = dummyLaps{1,12}(allTrials{1,12}(:,1) == tType,1);
-        %             outTrialsO{c,1} = allTrials{1,12}(allTrials{1,12}(:,1) == tType,:);
-        %             outDistsO{c,1} =  allDists{1,12}(allTrials{1,12}(:,1) == tType,1);
-        
-        for d = 1:length(Laps(1,:))
-            coefsO{s,d}{c} = coefs{1,d};
+        for d = 1:length(coefs(1,:))
+            coefsO{s,d}{1} = coefs{1,d};
+            featsO{s,d}{1} = allPredMat{1,d};
         end
         
         
-        clear Laps dummyLaps allTrials coefs allPredMat
-        
-        
-        %load(['/Volumes/Cerebro/Recording Data/',Name,'/OrganizedSessionInfo.mat'],'validSession','organizedTrials','timestampsPerTrial')
-        
-        %load(['/Volumes/Cerebro/Recording Data/',Name,'/Predict from Neurons/Neur-OutPredTime Cond_',num2str(intCondO),' TSeg_',num2str(eventO),' KWdth_',num2str(kWdthS),' BinSz_',num2str(binSize),' SB_',num2str(SB),'.mat'],'Distances')
-        
-        % Separate speed values by trial and delay type
+        clear  coefs  allPredMat
         
         s = s+1;
-        clearvars -except m ss  sessionsMap s coefsS coefsO
+        clearvars -except m ss  sessionsMap s coefsS coefsO featsO featsS
     end
 end
+
+
+d=12;
+
+for s = 1:length(coefsS(:,1))
+    
+    % get coefs
+    cS1{s} = coefsS{s,d}{1,1}(2:end);
+    cS2{s} = coefsS{s,d}{1,2}(2:end);
+    cO{s} = coefsO{s,d}{1,1}(2:end);
+    
+    % get frs
+    frS1{s} = featsS{s,d}{1,1};
+    frS2{s} = featsS{s,d}{1,2}; 
+    frO{s} = featsO{s,d}{1,1};
+    
+    % calculate correlations between coefs vectors
+    c1 = corrcoef(cS1{s},cS2{s});
+    c2 = corrcoef(cS1{s},cO{s});
+    c3 = corrcoef(cS2{s},cO{s});
+    
+    cS1S2(s) = c1(1,2);
+    cS1O(s) = c2(1,2);
+    cS2O(s) = c3(1,2);
+    
+    % calculate feature importance
+    
+    
+    % Stop trial speeds
+    allFrS1 = frS1{s};
+    allCoefS1 = cS1{s};
+    allImpS1 = sum(allFrS1 * abs(allCoefS1));
+    
+    % No Stop trial speeds
+    allFrS2 = frS2{s};
+    allCoefS2 = cS2{s};
+    allImpS2 = sum(allFrS2 * abs(allCoefS2));
+    
+    % Trial outcomes
+    allFrO = frO{s};
+    allCoefO = cO{s};
+    allImpO = sum(allFrO * abs(allCoefO));
+    
+    for n = 1:length(frS1{s}(1,:))
+        
+        % Stop trial speeds
+        nFrS1 = frS1{s}(:,n);
+        nCoefS1 = cS1{s}(n);
+        fImpS1 = sum(abs(nFrS1 * nCoefS1));
+        
+        
+        % No Stop trial speeds
+        nFrS2 = frS2{s}(:,n);
+        nCoefS2 = cS2{s}(n);
+        fImpS2 = sum(abs(nFrS2 * nCoefS2));
+        
+        
+        % Trial outcomes
+        nFrO = frO{s}(:,n);
+        nCoefO = cO{s}(n);
+        fImpO = sum(abs(nFrO * nCoefO));
+        
+        
+        featImp{s}(n,1) =  fImpS1 / allImpS1;
+        featImp{s}(n,2) =  fImpS2 / allImpS2;
+        featImp{s}(n,3) =  fImpO / allImpO;
+        
+        
+    end
+    
+end
+
+
+% calculate cumsums of feat imp and # of neurs untill treshold
+tresh = 0.8;
+for s = 1:16
+    
+    featImpS1{s} = cumsum(sort(featImp{s}(:,1),'descend'));
+    featImpS2{s} = cumsum(sort(featImp{s}(:,2),'descend'));
+    featImpO{s} = cumsum(sort(featImp{s}(:,3),'descend'));
+    
+    [~,treshNum(s,1)] = min(abs(featImpS1{s} - tresh));
+    [~,treshNum(s,2)] = min(abs(featImpS2{s} - tresh));
+    [~,treshNum(s,3)] = min(abs(featImpO{s} - tresh));
+    
+end
+
+
+% plot cumsum of feature importance of selected session
+
+s = 13;
+
+
+
+plot(featImpS1{s})
+hold on
+plot(featImpS2{s})
+hold on
+plot(featImpO{s})
+
+line([0 length(featImpS1{s})],[0.8 0.8],'Color','r','LineStyle','--')
+
+
+% plot mean +- SEM #neurs untill treshold
+
+bar(mean(treshNum))
+hold on 
+errorbar(mean(treshNum),std(treshNum)/sqrt(length(treshNum(:,1))))
+
+
+
+
+
+
